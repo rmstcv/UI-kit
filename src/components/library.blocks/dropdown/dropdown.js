82eeab5701e37e23f6cda946bb9d567b3dc0bd08
R <streltssov@gmail.com>
2021-11-29 23:55:42 +0500

    add Propagation in dropdown

--- src/components/library.blocks/dropdown/dropdown.js
+++ src/components/library.blocks/dropdown/dropdown.js
@@ -1,26 +1,25 @@
-
 handleCounter();
 handleHide();
 handleClear();
 
 function handleCounter () {
-  let dropdownCounter = document.querySelectorAll('.dropdown__counter');
+  const dropdownCounter = document.querySelectorAll('.dropdown');
   for(item of dropdownCounter) {
-    item.addEventListener('click' , (e) => {itemIterator(e.target), addText(e.target), addTexttoField()}); 
+    item.addEventListener('click' , (e) => {itemIterator(e.target), addTextToField(e.target), hideClearButton(e.target)}); 
   }
 }
 
 function handleHide () {
-  let dropdownMenu = document.querySelectorAll('.dropdown__field');
+  const dropdownMenu = document.querySelectorAll('.dropdown__field');
   for(item of dropdownMenu) {
-    item.addEventListener('click' , (e) => dropdownHide(e.target)); 
+    item.addEventListener('click' , (e) => {dropdownHide(e.target)}); 
   }
 }
 
 function handleClear () {
-  let dropdownClear = document.querySelectorAll('.dropdown__confirm-button_clear');
+  const dropdownClear = document.querySelectorAll('.dropdown__confirm-button_clear');
   for(item of dropdownClear) {
-    item.addEventListener('click' , (e) => dropdownClearValue(e.target)); 
+    item.addEventListener('click' , (e) => {dropdownClearValue(e.target)}); 
   }
 }
 
@@ -31,89 +30,177 @@ function dropdownHide (item) {
 }
 
 function itemIterator (item) {
-  let maxCount = 3;
-  let minCount = 0;
-  let count = item.closest('.dropdown__item-wrapper').getAttribute('data-dropdown-item-value');
-  count = Number(count);
-  if (item.classList.contains('dropdown__counter_next') && count < maxCount) {
-    count++;
-  } 
-  if (item.classList.contains('dropdown__counter_prev') && count > minCount) {
-    count--;
-  }
-
-  if (count == minCount ) {
+  if (item.classList.contains('dropdown__counter')) {
+    const maxCount = 3;
+    const minCount = 0;
+    let count = item.closest('.dropdown__item-wrapper').getAttribute('data-dropdown-item-value');
+    count = Number(count);
+    if (item.classList.contains('dropdown__counter_next') && count < maxCount) {
+      count++;
+    } 
+    if (item.classList.contains('dropdown__counter_prev') && count > minCount) {
+      count--;
+    }
+    if (count == minCount ) {
     item.parentNode.firstChild.classList.add('dropdown__counter_extreme-value');
-  } else {
-    item.parentNode.firstChild.classList.remove('dropdown__counter_extreme-value');
-  }
-  if (count == maxCount) {
-    item.parentNode.lastChild.classList.add('dropdown__counter_extreme-value');
-  } else {
-    item.parentNode.lastChild.classList.remove('dropdown__counter_extreme-value');
-  }
+    } else {
+      item.parentNode.firstChild.classList.remove('dropdown__counter_extreme-value');
+    }
+    if (count == maxCount) {
+      item.parentNode.lastChild.classList.add('dropdown__counter_extreme-value');
+    } else {
+      item.parentNode.lastChild.classList.remove('dropdown__counter_extreme-value');
+    }
 
   item.closest('.dropdown__item-counter').children[1].innerHTML = count;
   item.closest('.dropdown__item-wrapper').setAttribute('data-dropdown-item-value',  count);
-}
-
-function addText(item) {
-  let arr = [];
-  if (item.parentNode.classList.contains('dropdown')) {
-    while (!item.previousSibling.classList.contains('dropdown__field')) {
-      item = item.previousSibling
-    }
-    
-    for (i = 0; i < item.firstChild.children.length; i++) {
-      let name = item.firstChild.children[i].getAttribute('data-dropdown-item-name');
-      let value = item.firstChild.children[i].getAttribute('data-dropdown-item-value');
-      arr[i] = {name: name, value: value};
-    }
-    console.log(JSON.stringify(arr));
-    item.parentNode.setAttribute('data-dropdown-content', JSON.stringify(arr));
-    item.previousSibling.firstChild.innerHTML = `${arr[0].value} ${arr[0].name}, ${arr[1].value} ${arr[1].name}...`;
-  } 
-  else {
-    item = item.parentNode;
-    addText(item);
-  } 
+  }
 }
 
 function dropdownClearValue (item) {
- let elem = item.parentNode.previousSibling.children;
- for (i = 0; i < item.parentNode.previousSibling.children.length; i++) {
-  elem[i].lastChild.children[1].innerHTML = 0;
-  elem[i].setAttribute('data-dropdown-item-value',  0);
-  elem[i].lastChild.children[0].classList.add('dropdown__counter_extreme-value');
-  elem[i].lastChild.children[2].classList.remove('dropdown__counter_extreme-value');
+ const elems = item.parentNode.previousSibling.children;
+ for (i = 0; i < elems.length; i++) {
+  elems[i].lastChild.children[1].innerHTML = 0;
+  elems[i].setAttribute('data-dropdown-item-value',  0);
+  elems[i].lastChild.children[0].classList.add('dropdown__counter_extreme-value');
+  elems[i].lastChild.children[2].classList.remove('dropdown__counter_extreme-value');
  }
 }
 
-function addTexttoField() {
-  
-  let dropdownFields = document.querySelectorAll('.dropdown__field');
-  console.log(dropdownFields);
-
-  for (item of dropdownFields) {
-    let data = item.parentNode.getAttribute('data-dropdown-content');
-    
-    // item.firstChild.innerHTML = JSON.parse(data);
-    console.log(data);
+function addTextToField(item) {
+  if (item.classList.contains('dropdown__counter') || item.classList.contains('dropdown__confirm-button_clear')) {
+    const dropdown = item.closest('.dropdown');
+    const dropdownField = dropdown.children[1];
+    let arr = [];
+    let name, value;
+    const items = dropdownField.nextSibling.firstChild.children;
+    for (i = 0; i < items.length; i++) {
+      name = items[i].getAttribute('data-dropdown-item-name');
+      value = items[i].getAttribute('data-dropdown-item-value');
+      arr[i] = {name: name, value: value};
+    }
+    dropdown.setAttribute('data-dropdown-content', JSON.stringify(arr));
+    dropdownField.firstChild.innerHTML = `${arr[0].value} ${arr[0].name}, ${arr[1].value} ${arr[1].name}...`;
   }
-
-
-  // if (item.parentNode.classList.contains('dropdown')) {
-  //   for (i = 0; i < item.firstChild.children.length; i++) {
-  //     let name = item.firstChild.children[i].getAttribute('data-dropdown-item-name');
-  //     let value = item.firstChild.children[i].getAttribute('data-dropdown-item-value');
-  //     arr[i] = {name: name, value: value};
-  //   }
-  //   item.parentNode.setAttribute('data-dropdown-content', JSON.stringify(arr));
-  //   item.previousSibling.firstChild.innerHTML = `${arr[0].value} ${arr[0].name}, ${arr[1].value} ${arr[1].name}...`;
-  // } 
-  // else {
-  //   item = item.parentNode;
-  //   addText(item);
-  // } 
 }
 
+function hideClearButton (item) {
+  if (item.classList.contains('dropdown__counter') || item.classList.contains('dropdown__confirm-button_clear')) {
+    const dropdown = item.closest('.dropdown');
+    let sumItemsValues = 0;
+    const confirmPanel = item.closest('.dropdown__content-wrapper').lastChild;
+    const data = JSON.parse(dropdown.getAttribute('data-dropdown-content'));
+    for (value of data) {
+      sumItemsValues += +value.value;
+    }
+     if (confirmPanel) {
+       if (sumItemsValues != 0) {
+        confirmPanel.firstChild.classList.remove('dropdown__confirm-button_hide');
+       } else {
+         confirmPanel.firstChild.classList.add('dropdown__confirm-button_hide');
+       }
+     }
+  }
+}
+
+// handleCounter();
+// handleHide();
+// handleClear();
+
+// function handleCounter () {
+//   const dropdownCounter = document.querySelectorAll('.dropdown__counter');
+//   for(item of dropdownCounter) {
+//     item.addEventListener('click' , (e) => {itemIterator(e.target), addTextToField(), hideClearButton()}); 
+//   }
+// }
+
+// function handleHide () {
+//   const dropdownMenu = document.querySelectorAll('.dropdown__field');
+//   for(item of dropdownMenu) {
+//     item.addEventListener('click' , (e) => {dropdownHide(e.target)}); 
+//   }
+// }
+
+// function handleClear () {
+//   const dropdownClear = document.querySelectorAll('.dropdown__confirm-button_clear');
+//   for(item of dropdownClear) {
+//     item.addEventListener('click' , (e) => {dropdownClearValue(e.target), addTextToField(), hideClearButton()}); 
+//   }
+// }
+
+// function dropdownHide (item) {
+//   if(item.classList.contains('dropdown__check-wrapper')) {
+//     item.parentNode.nextSibling.classList.toggle('dropdown__content-wrapper_show')
+//   }
+// }
+
+// function itemIterator (item) {
+//   const maxCount = 3;
+//   const minCount = 0;
+//   let count = item.closest('.dropdown__item-wrapper').getAttribute('data-dropdown-item-value');
+//   count = Number(count);
+//   if (item.classList.contains('dropdown__counter_next') && count < maxCount) {
+//     count++;
+//   } 
+//   if (item.classList.contains('dropdown__counter_prev') && count > minCount) {
+//     count--;
+//   }
+
+//   if (count == minCount ) {
+//     item.parentNode.firstChild.classList.add('dropdown__counter_extreme-value');
+//   } else {
+//     item.parentNode.firstChild.classList.remove('dropdown__counter_extreme-value');
+//   }
+//   if (count == maxCount) {
+//     item.parentNode.lastChild.classList.add('dropdown__counter_extreme-value');
+//   } else {
+//     item.parentNode.lastChild.classList.remove('dropdown__counter_extreme-value');
+//   }
+
+//   item.closest('.dropdown__item-counter').children[1].innerHTML = count;
+//   item.closest('.dropdown__item-wrapper').setAttribute('data-dropdown-item-value',  count);
+// }
+
+// function dropdownClearValue (item) {
+//  const elems = item.parentNode.previousSibling.children;
+//  for (i = 0; i < elems.length; i++) {
+//   elems[i].lastChild.children[1].innerHTML = 0;
+//   elems[i].setAttribute('data-dropdown-item-value',  0);
+//   elems[i].lastChild.children[0].classList.add('dropdown__counter_extreme-value');
+//   elems[i].lastChild.children[2].classList.remove('dropdown__counter_extreme-value');
+//  }
+// }
+
+// function addTextToField() {
+//   const dropdownFields = document.querySelectorAll('.dropdown__field');
+//   for (item of dropdownFields) {
+//     let arr = [];
+//     let name, value;
+//     const items = item.nextSibling.firstChild.children;
+//     for (i = 0; i < items.length; i++) {
+//       name = items[i].getAttribute('data-dropdown-item-name');
+//       value = items[i].getAttribute('data-dropdown-item-value');
+//       arr[i] = {name: name, value: value};
+//     }
+//     item.parentNode.setAttribute('data-dropdown-content', JSON.stringify(arr));
+//     item.firstChild.innerHTML = `${arr[0].value} ${arr[0].name}, ${arr[1].value} ${arr[1].name}...`;
+//   }
+// }
+
+// function hideClearButton () {
+//   const items = document.querySelectorAll('.dropdown__content');
+//   for (item of items) {
+//     let sumItemsValues = 0;
+//     for (let i = 0; i < item.children.length; i++) {
+//       item.children[i].getAttribute('data-dropdown-item-value');
+//       sumItemsValues += Number(item.children[i].getAttribute('data-dropdown-item-value'));
+//     }
+//     if (item.nextSibling && sumItemsValues != 0) {
+//       item.nextSibling.firstChild.classList.remove('dropdown__confirm-button_hide');
+//     } else {
+//       if (item.nextSibling && sumItemsValues === 0) {
+//         item.nextSibling.firstChild.classList.add('dropdown__confirm-button_hide');
+//       }
+//     }
+//   }
+// }
