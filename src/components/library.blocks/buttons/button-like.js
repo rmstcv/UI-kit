5ddd644365fa669ddb2d9179093002b0a8290c75
R <streltssov@gmail.com>
2021-12-08 00:33:43 +0500

    like-button refactored

---
+++ src/components/library.blocks/buttons/button-like.js
@@ -0,0 +1,75 @@
+class LikeButton {
+  constructor(likeButton) {
+    this.likeButton = likeButton;
+    this.likes = +likeButton.getAttribute('data-likes');
+  }
+
+  getIsLike() {
+    const isliked = +this.likeButton.getAttribute('data-isLiked');
+    return isliked;
+  }
+
+  findElems(elemsClass) {
+    const elems = document.querySelectorAll(elemsClass);
+    const newElems = [];
+    let elem;
+    for (let i = 0; i < elems.length; i += 1) {
+      if (elems[i].closest('.js-like-button') === this.likeButton) {
+        newElems.push(elems[i]);
+      }
+    }
+    if (newElems.length !== 1) {
+      elem = newElems;
+    } else {
+      [elem] = newElems;
+    }
+    return elem;
+  }
+
+  checkLikes() {
+    const colored = this.findElems('.js-like-button__heart_colored');
+    const uncolored = this.findElems('.js-like-button__heart_uncolored');
+    if (this.getIsLike() === 1) {
+      colored.classList.add('like-button__heart_active');
+    } else {
+      uncolored.classList.add('like-button__heart_active');
+    }
+  }
+
+  addLikes() {
+    const numOfLikes = this.findElems('.js-like-button__number-of-likes');
+    numOfLikes.innerHTML = this.likes;
+  }
+
+  addLikeButtonHandler() {
+    this.likeButton.addEventListener('click', this.addLike);
+  }
+
+  addLike = () => {
+    const numOfLikes = this.findElems('.like-button__number-of-likes');
+    if (this.getIsLike() === 0) {
+      numOfLikes.innerHTML = this.likes + 1;
+      this.likes = this.likeButton.setAttribute('data-likes', this.likes + 1);
+      this.likeButton.setAttribute('data-isLiked', '1');
+    }
+    this.checkLikes();
+    this.likeButton.removeEventListener('click', this.addLike);
+  };
+
+  likeButtonInit() {
+    this.checkLikes();
+    this.addLikes();
+    this.addLikeButtonHandler();
+  }
+}
+
+function addLikeButton() {
+  const likeButtons = document.querySelectorAll('.js-like-button');
+  likeButtons.forEach((likeButtonItem) => {
+    let likeButton = likeButtonItem;
+    likeButton = new LikeButton(likeButton);
+    likeButton.likeButtonInit();
+  });
+}
+
+addLikeButton();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import 'jquery';
-import './components/library.blocks/buttons/buttons';
+import './components/library.blocks/buttons/button-like';
 import './components/library.blocks/dropdown/dropdown';
 import './components/library.blocks/checkboxList/checkboxList';
 import './components/library.blocks/nouislider/nouislider';
