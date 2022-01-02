6d8ddd700d4be7f6199a941ecd8c4da32c5634b9
R <streltssov@gmail.com>
2022-01-02 22:30:10 +0500

    add cards

--- src/blocks/card/card.js
+++ src/blocks/card/card.js
@@ -1,9 +1,23 @@
 import data from '../../data.json';
 import insertRoomInfo from '../room-short-info/room-short-info';
+import addStars from '../button-rate/button-rate';
 
 class Card {
-  constructor(room) {
-    this.room = room;
+  constructor(cardElem) {
+    this.room = cardElem.getAttribute('data-room');
+    this.cardRoom = this.getRoomData(this.room);
+    this.cardElem = cardElem;
+  }
+
+  findElem(elemClass) {
+    const elems = document.querySelectorAll(elemClass);
+    let elem;
+    for (let i = 0; i < elems.length; i += 1) {
+      if (elems[i].closest('.room-card') === this.cardElem) {
+        elem = elems[i];
+      }
+    }
+    return elem;
   }
 
   getRoomData() {
@@ -17,25 +31,36 @@ class Card {
     return roomInfo;
   }
 
-  addCard() {
-    const cards = document.querySelectorAll('.room-card');
-    let cardElem;
-    cards.forEach((elem) => {
-      if (elem.getAttribute('data-room') === this.room) cardElem = elem;
-    });
-    const cardRoom = this.getRoomData(this.room);
+  addCardImage() {
     const card = document.createElement('div');
-    card.classList.add('room-card');
-    const [img] = cardRoom.images;
+    card.classList.add('room-card__image');
+    const [img] = this.cardRoom.images;
     card.innerHTML = `<img src=${img} alt="image" loading="lazy" width="270">`;
-    cardElem.appendChild(card);
+    this.cardElem.prepend(card);
+  }
+
+  addStarsRate() {
+    const star = this.findElem('.rate-buttons');
+    star.setAttribute('data-rate', this.cardRoom.stars);
+    addStars(star);
+  }
+
+  addRoomRate() {
+    const rate = this.findElem('.room-card__rate-info');
+    rate.innerHTML = `${this.cardRoom.reviews} <span>Отзывов</span>`;
+  }
+
+  cardInit() {
+    this.addCardImage();
+    this.addRoomRate();
+    this.addStarsRate();
   }
 }
-
+Card.prototype.addStars = addStars;
 const cards = document.querySelectorAll('.room-card');
 cards.forEach((elem) => {
   const room = elem.getAttribute('data-room');
-  const card = new Card(room);
-  card.addCard();
-  insertRoomInfo(room, elem.firstElementChild);
+  const card = new Card(elem);
+  card.cardInit();
+  insertRoomInfo(room, elem.childNodes[1].childNodes[0]);
 });
